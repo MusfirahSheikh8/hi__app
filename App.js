d7c6161b52f0c37821b324d7a61afffc3ebb3038
MusfirahSheikh8 <musfirahsheikh1122@gmail.com>
2024-05-22 13:07:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+// import About from "./component/About";
+import Navbar from "./component/Navbar";
+import Text from "./component/Text";
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+function App() {
+  return (
+    <>
+      {/* <Router> */}
+      <Navbar title="ReactMagic" />
+      <div className="container" my-3="true">
+        {/* <Switch>
+            <Route path="/about">
+              <About />
+            </Route> */}
+        <Route path="/">
+          <Text heading="Enter your text below" />
+        </Route>
+        {/* </Switch> */}
+      </div>
+      {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
